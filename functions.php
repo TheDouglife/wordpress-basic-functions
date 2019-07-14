b682908f17d94d498e6e778808394f248e4c0ab9
Doug Montgomery <doug@douglife.com>
2019-07-14 14:03:48 -0400

    functions.php
    Added up to date scripts

---
+++ functions.php
@@ -0,0 +1,23 @@
+/*** Remove those crazy Wordpress emojis ***/
+
+remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+remove_action( 'wp_print_styles', 'print_emoji_styles' );
+remove_action( 'admin_print_styles', 'print_emoji_styles' );
+
+/*** Remove Contact Form 7 Javascript & CSS Crap ***/
+
+add_filter( 'wpcf7_load_js', '__return_false' );
+add_filter( 'wpcf7_load_css', '__return_false' );
+
+/*** Remove WP Version From Styles  ***/
+add_filter( 'style_loader_src', 'sdt_remove_ver_css_js', 9999 );
+// Remove WP Version From Scripts
+add_filter( 'script_loader_src', 'sdt_remove_ver_css_js', 9999 );
+
+/*** Function to remove version numbers  ***/
+function sdt_remove_ver_css_js( $src ) {
+	if ( strpos( $src, 'ver=' ) )
+		$src = remove_query_arg( 'ver', $src );
+	return $src;
+}
